ad56aa802f18d161b501b694a97583856c854aae
GianGeoffrey <giangervase@gmail.com>
2020-11-18 01:25:11 +0700

    18 Nov

--- TA/settings.py
+++ TA/settings.py
@@ -139,5 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+#please enter your own email, or put it on environment variables
 EMAIL_HOST_USER = os.environ.get('host_ta_email')
+#please its pass, or put it on environment variables
 EMAIL_HOST_PASSWORD = os.environ.get('host_ta_pass')
